e5123a68fd581b6ce9299414a49ae0959de32b73
Aurora <97414841+Aurhie@users.noreply.github.com>
2022-01-26 18:15:50 -0300

    MK-1

---
+++ Eclipse/Atividade5pt3.java
@@ -0,0 +1,27 @@
+package Familia44;
+
+import java.util.Scanner;
+
+public class Atividade5pt3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int segs,mins,hours;
+		
+		try (Scanner leia = new Scanner(System.in)) {
+			System.out.println("Quantos segundos irá durar? ");
+			segs=leia.nextInt();
+		}
+		
+		segs=segs/1;
+		mins=segs/60;
+		hours=mins/60;
+		
+		System.out.println("\nIrá durar "+hours+" Hora(s).");
+		System.out.println("\nIrá durar "+mins+"Minutos(s)");
+		System.out.println("\nIrá durar "+segs+" Segundo(s)");
+
+	}
+
+}
---
+++ Eclipse/Atividade5pt5.java
@@ -0,0 +1,33 @@
+package Familia44;
+
+import java.util.Scanner;
+
+public class Atividade5pt5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float nota1,nota2,nota3,media;
+		
+		try (Scanner leia = new Scanner(System.in)) {
+			System.out.println("Insira a nota 1: ");
+			nota1=leia.nextFloat();
+			System.out.println("\nInsira a nota 2: ");
+			nota2=leia.nextFloat();
+			System.out.println("\nInsira a nota 3: ");
+			nota3=leia.nextFloat();
+		}
+		
+		nota1=nota1*2;
+		nota2=nota2*3;
+		nota3=nota3*5;
+		
+		media=(nota1+nota2+nota3)/10;
+		
+		System.out.println("\nMédia Aritimédica é : "+media);
+		
+				
+
+	}
+
+}
